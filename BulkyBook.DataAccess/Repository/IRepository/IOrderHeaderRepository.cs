94bcc78642c6b4ee5b4ea8fd4055bfdad1028094
prudvi <prudvi14284@gmail.com>
2023-01-02 20:04:37 +0530

    Update new code

--- BulkyBook.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -10,6 +10,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 	public interface IOrderHeaderRepository : IRepository<OrderHeader>
 	{
 		void Update(OrderHeader obj);
+
 		void UpdateStatus(int id, string OrderStatus, string? PaymentStatus = null);
+		void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId );
+		
 	}
 }
--- BulkyBook/Program.cs
+++ BulkyBook/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
+	.AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
@@ -45,7 +46,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe : SecretKey").Get<string>();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
 app.UseAuthentication();
 
