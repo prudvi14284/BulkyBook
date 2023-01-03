6a63d4cc1550d16635183fc4b91c16930183bb42
prudvi <prudvi14284@gmail.com>
2023-01-03 17:25:38 +0530

    Update new code

---
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,65 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize]
+	public class OrderController : Controller
+	{
+		//Dependency Injection
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaders;
+
+			if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+			{
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+			else
+			{
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value,includeProperties: "ApplicationUser");
+            }
+            
+
+            switch (status)
+            {
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
+		}
+		#endregion
+	}
+}
