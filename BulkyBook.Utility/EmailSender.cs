d04b6c197507191276fad301e067168ba6de493c
prudvi <prudvi14284@gmail.com>
2023-01-06 19:47:06 +0530

    Update new code

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -10,16 +10,11 @@ namespace BulkyBook.Utility
 {
 	public class EmailSender : IEmailSender
 	{
-		//public string SendGridSecret { get; set; }
-
-		//public EmailSender(IConfiguration _config)
-		//{
-		//	SendGridSecret = _config.GetValue<string>("SendGrid : SecretKey");
-		//}
+		
 		public Task SendEmailAsync(string email, string subject, string htmlMessage)
 		{
 			//var emailToSend = new MimeMessage();
-			//emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+			//emailToSend.From.Add(MailboxAddress.Parse(""));
 			//emailToSend.To.Add(MailboxAddress.Parse(email));
 			//emailToSend.Subject = subject;
 			//emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
@@ -28,17 +23,11 @@ namespace BulkyBook.Utility
 			//using (var emailClient = new SmtpClient())
 			//{
 			//	emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-			//	emailClient.Authenticate("prudvi14284@gmail.com", "8179577923");
+			//	emailClient.Authenticate("");
 			//	emailClient.Send(emailToSend);
 			//	emailClient.Disconnect(true);
 			//}
 			//return Task.CompletedTask;
-
-			//var client = new SendGridClient(SendGridSecret);
-			//var from = new EmailAddress("yprudvi@msystechnologies.com", "Bulky Book");
-			//var to = new EmailAddress(email);
-			//var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-			//return client.SendEmailAsync(msg);
 			return Task.CompletedTask;
         }
 	}
